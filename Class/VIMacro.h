42e50cf42e743b476316ac5e5f875308850d99e1
Vienta <shuyx@danqoo.com>
2016-09-22 16:11:23 +0800

    second commit

---
+++ Class/VIMacro.h
@@ -0,0 +1,13 @@
+//
+//  VIMacro.h
+//  VIUtilsKit
+//
+//  Created by Vienta on 9/22/16.
+//  Copyright © 2016 Vienta. All rights reserved.
+//
+
+#ifndef VIMacro_h
+#define VIMacro_h
+
+
+#endif /* VIMacro_h */
