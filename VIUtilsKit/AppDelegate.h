c8c5af2134b22e709607cea28e785cf1064aeb9f
Vienta <shuyx@danqoo.com>
2016-09-22 15:48:57 +0800

    first commit

---
+++ VIUtilsKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VIUtilsKit
+//
+//  Created by Vienta on 9/22/16.
+//  Copyright © 2016 Vienta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VIUtilsKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VIUtilsKit
+//
+//  Created by Vienta on 9/22/16.
+//  Copyright © 2016 Vienta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
